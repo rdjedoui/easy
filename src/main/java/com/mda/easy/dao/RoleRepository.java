ad33a2e636c36ec451a2ae54a1c1a651cfaa882f
rdjedoui <rafika.djedoui@gmail.com>
2019-05-08 05:44:40 +0200

    fix compilation problems

--- src/main/java/com/mda/easy/dao/RoleRepository.java
+++ src/main/java/com/mda/easy/dao/RoleRepository.java
@@ -2,6 +2,8 @@ package com.mda.easy.dao;
 
 import com.mda.easy.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
